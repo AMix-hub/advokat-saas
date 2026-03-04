f371862382c1ca1ea4de0028e09a767afae0ace6
Mats Tongring <matztongring@gmail.com>
2026-03-04 09:34:07 +0100

    Uppdaterat Next.js och inaktiverat ESLint vid build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // Säger åt Vercel att ignorera linting-fel när den bygger systemet
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
